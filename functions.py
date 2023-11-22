7ac9b1ef94110d4b26b80269446a2c80878eb29d
ShatterdPotato <saahmed5526@gmail.com>
2023-11-21 20:32:32 -0500

    Fixed pygame, started setting up file strucutre and stuff like that

---
+++ functions.py
@@ -0,0 +1,5 @@
+def test_run():
+    print("TEST")
+
+def test_run_2():
+    print("test 2")
--- greatness.py
+++ greatness.py
@@ -1,8 +1,10 @@
+from functions import *
 import pygame
 import os
 
 WIDTH, HEIGHT = 800, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("PLACEHOLDER FOR CAPTIONS LATER")
 
 def main():
     run = True
@@ -10,4 +12,6 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
+main() 
+test_run()
+test_run_2()
