2cc6a70c8644ca39dbe99452db8befd2c0ff9139
Sameer <ShatterdPotato>
2023-12-31 23:44:37 -0500

    fixed gravity, half way done with jump mechanic

--- greatness.py
+++ greatness.py
@@ -5,16 +5,17 @@ import os
 WIDTH, HEIGHT = 900, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("PLACEHOLDER FOR CAPTIONS LATER")
+GRAVITY = 1
 
 FPS = 60
 WHITE = (255, 255, 255)
 
-#Characters here
-player_width, player_height = 55,100
+# Characters here
+player_width, player_height = 55, 100
 derp_dude = pygame.image.load(os.path.join('Assets', 'derp_dude.png'))
 rage_dude = pygame.image.load(os.path.join('Assets', 'rage_dude.png'))
-derp_dude_rect = pygame.Rect(100, 300, player_width, player_height)
-rage_dude_rect = pygame.Rect(100, 300, player_width, player_height)
+derp_dude_rect = pygame.Rect(100, 0, player_width, player_height)
+rage_dude_rect = pygame.Rect(100, 0, player_width, player_height)
 
 
 def draw_update():
@@ -23,23 +24,29 @@ def draw_update():
     import_player(rage_dude, WIN, rage_dude_rect)
     pygame.display.update()
 
+
 def main():
     clock = pygame.time.Clock()
     run = True
+    velocity_derp = 0
+    velocity_rage = 0
+    jumping = False
     while run:
+        keys_pressed = pygame.key.get_pressed()
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.KEYDOWN:
+                if keys_pressed[pygame.K_w]:
+                    jumping = True
+                    print("YO")
 
-            while event.type == pygame.KEYDOWN:
-                if rage_dude_rect.bottom == 600:
-                    if event.key == pygame.K_w:
-                        gravity(pygame.K_w, rage_dude_rect)
-                        jump(rage_dude_rect)
-                    
-        character_movement(derp_dude, derp_dude_rect, pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN)
-        character_movement(rage_dude, rage_dude_rect, pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s)
+        velocity_derp = character_movement(derp_dude, derp_dude_rect, pygame.K_LEFT,
+                                           pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN, HEIGHT, velocity_derp, jumping)
+        velocity_rage = character_movement(rage_dude, rage_dude_rect,
+                                           pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s, HEIGHT, velocity_rage, jumping)
         draw_update()
-        
-main() 
+
+
+main()
